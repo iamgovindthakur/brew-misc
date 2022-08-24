2d2231b53000c4eed1e1dc8f7d2980e5d96bf2d9
iamgovindthakur <iamgovindthakur@gmail.com>
2022-08-24 20:46:21 +0530

    initial commit

---
+++ src/main/java/com/iamgkt/brewer/BrewMicApplication.java
@@ -0,0 +1,13 @@
+package com.iamgkt.brewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewMicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrewMicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iamgkt/brewmic/BrewMicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamgkt.brewmic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewMicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
